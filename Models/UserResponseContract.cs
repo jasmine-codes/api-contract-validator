0b530842f35acb49f06b1905cb726eb7e323d9c3
Jasmine Lee <jasminelee@Jasmines-MacBook-Pro.local>
2025-07-25 06:53:00 +0200

    Initial commit: API Contract Validator setup

---
+++ Models/UserResponseContract.cs
@@ -0,0 +1,11 @@
+namespace ApiContractValidator.Models
+{
+    public class UserResponseContract
+    {
+        public int id {get; set;}
+        public string name {get; set;}
+        public string email {get; set;}
+    }
+}
+
+//continue 6. API Caller Service
---
+++ UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace api_contract_validator;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
