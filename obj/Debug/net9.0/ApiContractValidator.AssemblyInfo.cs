389cad56f91e04aacd715db31ff1b68f07991566
jasmine-codes <jsmlee97@gmail.com>
2025-07-30 07:35:11 +0200

    load expected values from JSON files

--- obj/Debug/net9.0/ApiContractValidator.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiContractValidator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiContractValidator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca9ed403a35562ca088d0d51bfa7e00879f907b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40ddaa08819cc3a494fccf0b1091b2bbd0c77ccc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiContractValidator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiContractValidator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
