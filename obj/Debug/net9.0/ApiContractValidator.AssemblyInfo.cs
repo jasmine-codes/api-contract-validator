e97596fba915b671c5f5a676bd7cc1e515f4273f
jasmine-codes <jsmlee97@gmail.com>
2025-08-01 07:38:33 +0200

    cleanup

--- obj/Debug/net9.0/ApiContractValidator.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiContractValidator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiContractValidator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bec1b6e6f8deb29203c877f285a36dcbd756971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab32f9ef5908c221b31a7108c4efb9fce8f2835")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiContractValidator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiContractValidator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
